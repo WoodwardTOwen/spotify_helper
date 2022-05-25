fd7c0abe38348ef157907fedd55063a6c317ff98
Owen Woodward <owen.woodward@nttdata.com>
2022-05-25 11:16:45 +0100

    Added fix so the retrieving the current user now has some error handling to prevent some nasty use cases

--- lib/screens/playlists_screen.dart
+++ lib/screens/playlists_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
-import 'package:spotify_helper/util/helper_methods.dart';
-
 import '../Http/bloc/playlist/playlist_bloc_bloc.dart';
 import '../models/user_model.dart';
 import '../providers/user_provider.dart';
@@ -27,34 +25,37 @@ class PlaylistsScreen extends StatefulWidget {
 
 class _PlaylistsScreenState extends State<PlaylistsScreen> {
   bool _isLoading = true;
-  late UserModel _currentUser;
+  UserModel? _currentUser;
 
   late PlaylistBloc _playlistBloc;
   final ScrollController _controller =
       ScrollController(initialScrollOffset: 0.0, keepScrollOffset: true);
   var isFiltered = false;
 
-  //Add controller here
-
   void _onScrolledToEnd() {
     var isEnd = _controller.offset == _controller.position.maxScrollExtent;
     if (isEnd) _playlistBloc.add(MyPlaylistsFetchedEvent());
   }
 
-  @override
-  void initState() {
+  void _attemptToRetrieveCurrentUser() {
     Provider.of<UserProvider>(context, listen: false)
         .getCurrentUser
         .catchError((onError) {
-      print(onError
-          .toString()); //TODO Handle this occurance -> Re-Route to another page?
+      setState(() {
+        _isLoading = false;
+        _currentUser = null;
+      });
     }).then((value) {
       setState(() {
         _isLoading = false;
         _currentUser = value;
       });
     });
+  }
 
+  @override
+  void initState() {
+    _attemptToRetrieveCurrentUser();
     _playlistBloc = BlocProvider.of<PlaylistBloc>(context);
     _controller.addListener(_onScrolledToEnd);
     super.initState();
@@ -68,6 +69,29 @@ class _PlaylistsScreenState extends State<PlaylistsScreen> {
 
   Widget _createErrorMessage(String errorMessage) {
     return Text(errorMessage, style: const TextStyle(color: Colors.black));
+  }
+
+  Widget _createErrorWidget() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            "Error trying to retrieve User, \n Would you like to retry?",
+            style: TextStyle(
+              color: Colors.black,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          ElevatedButton(
+              child: const Text("Retry"),
+              onPressed: () => _attemptToRetrieveCurrentUser())
+        ],
+      ),
+    );
   }
 
   Future<void> _onRefresh() async {
@@ -97,81 +121,76 @@ class _PlaylistsScreenState extends State<PlaylistsScreen> {
                 )
               ],
             ),
-            drawer: SpotifyHelperDrawer(user: _currentUser),
-            body: BlocBuilder<PlaylistBloc, PlaylistBlocState>(
-              builder: (ctx, state) {
-                if (state is PlaylistBlocLoadingState) {
-                  return const Center(child: CircularProgressIndicator());
-                }
-                if (state is PlaylistWithData) {
-                  final playlists = state.playlists;
-                  return BlocListener<PlaylistBloc, PlaylistBlocState>(
-                    listenWhen: (previous, current) =>
-                        previous is PlaylistRefreshingState,
-                    listener: (context, state) {
-                      if (state is PlaylistBlocLoaded) {
-                        HelperMethods.createSnackBarMessage(
-                            context, "Finished Refreshing");
+            drawer: _currentUser == null
+                ? null
+                : SpotifyHelperDrawer(user: _currentUser!),
+            body: _currentUser == null
+                ? _createErrorWidget()
+                : BlocBuilder<PlaylistBloc, PlaylistBlocState>(
+                    builder: (ctx, state) {
+                      if (state is PlaylistBlocLoadingState) {
+                        return const Center(child: CircularProgressIndicator());
                       }
-                    },
-                    child: RefreshIndicator(
-                      onRefresh: () async => _onRefresh(),
-                      child: Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: Column(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.only(bottom: 5),
-                              child: Text(
-                                isFiltered
-                                    ? "Playlists by ${_currentUser.displayName}"
-                                    : "All Saved Playlists",
-                                style: const TextStyle(
-                                    color: Colors.black, fontSize: 18),
-                              ),
+                      if (state is PlaylistWithData) {
+                        final playlists = state.playlists;
+                        return RefreshIndicator(
+                          onRefresh: () async => _onRefresh(),
+                          child: Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: Column(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.only(bottom: 5),
+                                  child: Text(
+                                    isFiltered
+                                        ? "Playlists by ${_currentUser!.displayName}"
+                                        : "All Saved Playlists",
+                                    style: const TextStyle(
+                                        color: Colors.black, fontSize: 18),
+                                  ),
+                                ),
+                                playlists.isEmpty
+                                    ? const Center(
+                                        child: Text("No Playlists found!!"),
+                                      )
+                                    : Expanded(
+                                        child: GridView.builder(
+                                          controller: _controller,
+                                          itemBuilder: ((ctx, index) {
+                                            return PlayListTileGridView(
+                                              isFiltered
+                                                  ? state.getFilteredResult(
+                                                      _currentUser!
+                                                          .userId)[index]
+                                                  : state.playlists[index],
+                                            );
+                                          }),
+                                          itemCount: isFiltered
+                                              ? state
+                                                  .getFilteredResult(
+                                                      _currentUser!.userId)
+                                                  .length
+                                              : state.playlists.length,
+                                          gridDelegate:
+                                              const SliverGridDelegateWithFixedCrossAxisCount(
+                                            crossAxisCount: 2,
+                                            childAspectRatio: 3 / 2,
+                                            crossAxisSpacing: 10,
+                                            mainAxisSpacing: 10,
+                                          ),
+                                        ),
+                                      )
+                              ],
                             ),
-                            playlists.isEmpty
-                                ? const Center(
-                                    child: Text("No Playlists found!!"),
-                                  )
-                                : Expanded(
-                                    child: GridView.builder(
-                                      controller: _controller,
-                                      itemBuilder: ((ctx, index) {
-                                        return PlayListTileGridView(
-                                          isFiltered
-                                              ? state.getFilteredResult(
-                                                  _currentUser.userId)[index]
-                                              : state.playlists[index],
-                                        );
-                                      }),
-                                      itemCount: isFiltered
-                                          ? state
-                                              .getFilteredResult(
-                                                  _currentUser.userId)
-                                              .length
-                                          : state.playlists.length,
-                                      gridDelegate:
-                                          const SliverGridDelegateWithFixedCrossAxisCount(
-                                        crossAxisCount: 2,
-                                        childAspectRatio: 3 / 2,
-                                        crossAxisSpacing: 10,
-                                        mainAxisSpacing: 10,
-                                      ),
-                                    ),
-                                  )
-                          ],
-                        ),
-                      ),
-                    ),
-                  );
-                } else if (state is PlaylistFailureState) {
-                  return _createErrorMessage(
-                      'Something Went Wrong: ${state.error.toString()}, \n Please Try Again');
-                }
-                return Container();
-              },
-            ),
+                          ),
+                        );
+                      } else if (state is PlaylistFailureState) {
+                        return _createErrorMessage(
+                            'Something Went Wrong: ${state.error.toString()}, \n Please Try Again');
+                      }
+                      return Container();
+                    },
+                  ),
           );
   }
 }
