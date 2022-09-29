98cc7da2e615789068a349ea538b07d09ffe14ef
Owen Woodward <owen.woodward@nttdata.com>
2022-09-29 14:56:14 +0100

    Added functionality to create a playback session

--- lib/screens/track_details_screen.dart
+++ lib/screens/track_details_screen.dart
@@ -4,9 +4,8 @@ import 'package:spotify_helper/models/track_details_model.dart';
 import 'package:spotify_helper/providers/tracks_provider.dart';
 import 'package:spotify_helper/widgets/misc/network_image.dart';
 import 'package:spotify_helper/widgets/track_stats/percentage_indicator_widget.dart';
+import 'package:spotify_helper/widgets/track_stats/recommended_tracks.dart';
 import 'package:spotify_helper/widgets/track_stats/track_stats.dart';
-
-import '../widgets/user_stats_widgets/track_tile.dart';
 
 class TrackDetailPage extends StatefulWidget {
   static const routeName = '/track-details';
@@ -29,19 +28,16 @@ class _TrackDetailPageState extends State<TrackDetailPage> {
   Future<void> _getTrackDetails(String trackId) async {
     await _trackProvider.getTrackById(trackId: trackId);
     await _trackProvider.getAudioFeaturesByTrackId(trackId: trackId);
-    await _trackProvider.getArtistGenresByTrackId(
-        artistId: _trackProvider.getTrackDetails()!.artistID[0]);
-    await _trackProvider.getRecommendationsForTrack();
   }
 
   Widget _createTitle(TrackDetailsModel trackDetailsModel) => Container(
-        margin: EdgeInsets.only(top: 10),
+        margin: const EdgeInsets.only(top: 10),
         child: Column(
           children: [
             Text(
               trackDetailsModel.trackName,
               textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 14, color: Colors.black87),
+              style: const TextStyle(fontSize: 14, color: Colors.black87),
             ),
             const Text(
               "By",
@@ -49,7 +45,7 @@ class _TrackDetailPageState extends State<TrackDetailPage> {
             ),
             Text(trackDetailsModel.artist,
                 textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 14, color: Colors.black87)),
+                style: const TextStyle(fontSize: 14, color: Colors.black87)),
           ],
         ),
       );
@@ -59,9 +55,19 @@ class _TrackDetailPageState extends State<TrackDetailPage> {
     final trackIdArgs = ModalRoute.of(context)!.settings.arguments as String;
 
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        actions: [
+          IconButton(
+              icon: const Icon(
+                Icons.play_arrow,
+                color: Colors.white,
+                size: 25,
+              ),
+              onPressed: () => _trackProvider.createPlayback(
+                  trackId: _trackProvider.getTrackDetails()!.trackId)),
+        ],
+      ),
       backgroundColor: const Color.fromRGBO(239, 234, 216, 1),
-      //backgroundColor: const Color.fromRGBO(128, 161, 212, 1),
       body: FutureBuilder(
         future: _getTrackDetails(trackIdArgs),
         builder: (ctx, snapshot) => snapshot.connectionState ==
@@ -93,25 +99,14 @@ class _TrackDetailPageState extends State<TrackDetailPage> {
                             _trackProvider.getTrackDetails()!.popularity,
                       ),
                       Container(
-                        padding: EdgeInsets.only(top: 8),
-                        child: Text(
+                        padding: const EdgeInsets.only(top: 8),
+                        child: const Text(
                           "Related Tracks",
                           textAlign: TextAlign.center,
                           style: TextStyle(fontSize: 13, color: Colors.black87),
                         ),
                       ),
-                      Consumer<TrackProvider>(
-                        builder: (ctx, data, _) => ListView.builder(
-                          shrinkWrap: true,
-                          physics: const NeverScrollableScrollPhysics(),
-                          padding: EdgeInsets.zero,
-                          itemBuilder: ((ctx, index) => TrackTile(
-                                trackItem: data.currentRecommendedTracks[index],
-                                indexValue: index + 1,
-                              )),
-                          itemCount: data.currentRecommendedTracks.length,
-                        ),
-                      ),
+                      const RecommendedTracks()
                     ],
                   ),
                 ),
